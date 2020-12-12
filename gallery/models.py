b0982333ec5fb9736efc49a1136e579238ed87cc
Kirsty Harper <kirstyharper@kirstys-mbp.home>
2020-12-12 18:06:50 +0000

    fix(gallery): json formatting

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.ImageField()
+    location = models.CharField(max_length=500, null=True, blank=True)
+    caption = models.TextField(null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
