d782fd07b63380065da3aa265529952b9bebf7aa
Kirsty Harper <kirstyharper@kirstys-mbp.home>
2020-12-15 13:28:35 +0000

    fix(github): push issues

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Image
 
 
 def view_gallery(request):
     """ view for gallery page """
-    return render(request, 'gallery/gallery.html')
+    images = Image.objects.all()
+    context = {
+        'images': images,
+    }
+    return render(request, 'gallery/gallery.html', context)
--- rpoas_lanarkshire/settings.py
+++ rpoas_lanarkshire/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'home',
     'gallery',
     # Sass
-    'django_sass'
+    'django_sass',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -149,7 +150,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = '/static/'
+SASS_PROCESSOR_ROOT = STATIC_ROOT
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
