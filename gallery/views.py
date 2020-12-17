b86885fc7f610a7861775757028bbf59e60a5fe7
Kirsty Harper <kirsty.harper@zoho.com>
2020-12-17 22:05:32 +0000

    feat(gallery): search by title and caption

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Image
 
 
 def view_gallery(request):
     """ view for gallery page """
     images = Image.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'Please enter a search term into the search box.')
+                return redirect(reverse('gallery'))
+            
+            queries = Q(title__icontains=query) | Q(caption__icontains=query)
+            images = images.filter(queries)
+
     context = {
         'images': images,
     }
+    
     return render(request, 'gallery/gallery.html', context)
