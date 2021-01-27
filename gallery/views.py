c7d9c402bfe220c49bd14ba5ca1f5dc2cd2288aa
Kirsty Harper <kirsty.harper@zoho.com>
2021-01-27 22:26:15 +0000

    fix(gallery): filters work together

--- gallery/views.py
+++ gallery/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Image
 
 
 def view_gallery(request):
     """ view for gallery page """
-    images = Image.objects.all()
+    images = Image.objects.order_by('location')
+    query = None
     locations = None
     filterLocation = None
-    query = None
+    sort = None
+    direction = None
 
     if request.GET:
         if 'q' in request.GET:
@@ -24,19 +27,33 @@ def view_gallery(request):
         
         if 'location' in request.GET:
             filterLocation = request.GET['location'].split(',')
-            images = images.filter(location__name__in=filterLocation)
-            filterLocation = Product.objects.filter(name__in=product.location)
+            images = images.filter(location__in=filterLocation)
+            filterLocation = Image.objects.filter(location__in=images)
         
-
-        # if 'location' in request.GET:
-        #     """ filter by location """
-        #     filterLocation = request.GET['location'].split(',')
-        #     images = images.filter(image__location__in=filterLocation)
-
-
+        # if 'year' in request.GET:
+        #     filterYear = request.GET['year'].split(',')
+        #     images = images.filter(year__in=filterYear)
+        #     filterYear = Image.objects.filter(year__in=images)
+        
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'location':
+                sortkey = 'lower_location'
+                images = images.annotate(lower_location=Lower('location'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            images = images.order_by(sortkey)
+        
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'images': images,
+        'search_term': query,
+        'current_location': locations,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'gallery/gallery.html', context)
