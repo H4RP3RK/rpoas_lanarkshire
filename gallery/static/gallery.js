0c66efd04f4712c0ce9ab4623829425d1eb17d9a
Kirsty Harper <kirsty.harper@zoho.com>
2021-02-04 21:21:42 +0000

    feat(gallery): modal arrows onclick

--- gallery/static/gallery.js
+++ gallery/static/gallery.js
@@ -1,8 +1,23 @@
-// Toggles filter buttons
+// Toggles filter buttons and modal
 
-function toggle(btnName) {
-    const button = document.querySelector(`#${btnName}`);
-    const displayList = button.nextElementSibling;
-    displayList.classList.toggle("closed");
+function toggle(elName) {
+    const el = document.querySelector(`#${elName}`);
+    const displayList = el.nextElementSibling;
+    displayList.classList.toggle('closed');
 }
 
+// Modal arrows
+
+function nextImage(openModal) {
+    const modal = document.querySelector(`#${openModal}`);
+    const nextModal = modal.nextElementSibling.nextElementSibling;
+    modal.classList.add('closed');
+    nextModal.classList.toggle('closed');
+}
+
+function previousImage(openModal) {
+    const modal = document.querySelector(`#${openModal}`);
+    const previousModal = modal.previousElementSibling.previousElementSibling;
+    modal.classList.add('closed');
+    previousModal.classList.toggle('closed');
+}
