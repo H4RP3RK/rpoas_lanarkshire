9014dd9e10247b95ec4f2e202e6893defef16334
Kirsty Harper <kirsty.harper@zoho.com>
2021-02-15 21:20:55 +0000

    feat(contact): add form

---
+++ contact/static/contact.js
@@ -0,0 +1,20 @@
+
+function sendMail(contactForm) {
+    const centerForm = document.querySelector(".center-form");
+    
+    emailjs.send("gmail", "rpoas", {
+        "from_name": contactForm.fullName.value,
+        "from_email": contactForm.emailAddress.value,
+        "email_details": contactForm.query.value,
+    })
+    .then(
+        function(response) {
+            console.log('SUCCESS!', response);
+            centerForm.innerHTML = `<h1 class="email-message text-center form-section">Thanks for your email, ${contactForm.fullName.value}. We'll be in touch soon.<br><i class="far fa-thumbs-up"></i></h1>`;
+        }, 
+        function(error) {
+            console.log('FAILED...', error);
+            centerForm.innerHTML = `<h1 class="email-message text-center form-section">Sorry ${contactForm.fullName.value}. There's seems to be a problem. Please try to send the email again</h1>`;
+        });
+    return false;
+}
